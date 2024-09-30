fa7b8817620dadaa5f312afbb4189fab4f9c1624
proadhikary <prottay71@gmail.com>
2024-09-30 21:43:06 +0530

    part A

--- deep_embedding.py
+++ deep_embedding.py
@@ -1,3 +1,4 @@
+# pip install umap-learn
 # pip uninstall torch-scatter torch-sparse torch-geometric torch-cluster  --y
 # pip install torch-scatter -f https://data.pyg.org/whl/torch-{torch.__version__}.html
 # pip install torch-sparse -f https://data.pyg.org/whl/torch-{torch.__version__}.html
